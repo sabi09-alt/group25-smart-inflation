6e87686e72d7ea2190947ec1d8b7c7d4259d661b
sabi09-alt <aliceringo149@gmail,com>
2026-02-16 09:00:42 +0300

    Initial commit

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load("model.pkl")
+
+st.set_page_config(page_title="Smart Inflation Forecasting System", layout="centered")
+
+st.title("📈 Smart Inflation Forecasting System")
+st.markdown("Predict Tanzania's Inflation Rate using Machine Learning")
+
+st.write("---")
+
+st.subheader("Enter Economic Indicators")
+
+gdp_growth = st.number_input("GDP Growth Rate (%)", min_value=-10.0, max_value=20.0, value=5.0)
+interest_rate = st.number_input("Interest Rate (%)", min_value=0.0, max_value=30.0, value=10.0)
+exchange_rate = st.number_input("Exchange Rate (TZS per USD)", min_value=1000.0, max_value=5000.0, value=2500.0)
+unemployment = st.number_input("Unemployment Rate (%)", min_value=0.0, max_value=50.0, value=10.0)
+
+st.write("---")
+
+if st.button("Predict Inflation Rate"):
+
+    # Prepare input data
+    input_data = np.array([[gdp_growth, interest_rate, exchange_rate, unemployment]])
+
+     # Make prediction
+    prediction = model.predict(input_data)
+
+    # Display result
+    st.success(f"Predicted Inflation Rate: {prediction[0]:.2f}%")
+
+    # Interpretation
+    if prediction[0] < 5:
+        st.info("Inflation is considered LOW and stable.")
+    elif prediction[0] < 10:
+        st.warning("Inflation is MODERATE.")
+    else:
+        st.error("Inflation is HIGH. Economic pressure may increase.")
+
+    
+
+
+
+
+
+
+
